effa1339c8ab513f14c1ee489169d3d752173812
Dan Book <grinnz@grinnz.com>
2014-11-29 18:09:35 -0500

    fix pod formatting

--- lib/Mojo/JSON/MaybeXS.pm
+++ lib/Mojo/JSON/MaybeXS.pm
@@ -34,39 +34,63 @@ Mojo::JSON::MaybeXS - use JSON::MaybeXS as the JSON encoder for Mojolicious
 
 =head1 DESCRIPTION
 
-L<Mojo::JSON::MaybeXS> is a monkey-patch module for using L<JSON::MaybeXS> in place of L<Mojo::JSON> in a L<Mojolicious> application. It must be loaded before Mojolicious so the new functions will be properly exported.
+L<Mojo::JSON::MaybeXS> is a monkey-patch module for using L<JSON::MaybeXS> in
+place of L<Mojo::JSON> in a L<Mojolicious> application. It must be loaded
+before Mojolicious so the new functions will be properly exported.
 
 =head1 CAVEATS
 
-L<JSON::MaybeXS> may load different modules depending on what is available, and these modules have slightly different behavior from each other and from L<Mojo::JSON>. As of this writing, the author has found the following incompatibilities:
+L<JSON::MaybeXS> may load different modules depending on what is available, and
+these modules have slightly different behavior from each other and from
+L<Mojo::JSON>. As of this writing, the author has found the following
+incompatibilities:
 
 =head2 Boolean Stringification
 
-If L<Cpanel::JSON::XS> is loaded by L<JSON::MaybeXS> (the default if available), the L<Mojo::JSON(true)> and L<Mojo::JSON(false)> booleans will stringify to C<"true"> and C<"false">. However, when using L<JSON::XS>, or L<JSON::PP>, they will stringify to C<"1"> or C<"0">, like in L<Mojo::JSON>.
+If L<Cpanel::JSON::XS> is loaded by L<JSON::MaybeXS> (the default if available),
+the L<Mojo::JSON(true)> and L<Mojo::JSON(false)> booleans will stringify to
+C<"true"> and C<"false">. However, when using L<JSON::XS>, or L<JSON::PP>, they
+will stringify to C<"1"> or C<"0">, like in L<Mojo::JSON>.
 
 =head2 Object Conversion
 
-Both L<JSON::MaybeXS> and L<Mojo::JSON> will attempt to call the TO_JSON method of a blessed reference to produce a JSON-friendly structure. If that method does not exist, L<JSON::MaybeXS> will encode the object to C<null>, while L<Mojo::JSON> will stringify the object.
+Both L<JSON::MaybeXS> and L<Mojo::JSON> will attempt to call the TO_JSON method
+of a blessed reference to produce a JSON-friendly structure. If that method
+does not exist, L<JSON::MaybeXS> will encode the object to C<null>, while
+L<Mojo::JSON> will stringify the object.
 
 =head2 Unblessed References
 
-L<JSON::MaybeXS> does not allow unblessed references other than hash and array references or references to the integers C<0> and C<1>, and will throw an exception if attempting to encode one. L<Mojo::JSON> will treat scalar references as references to C<0> or C<1> and will encode them to C<true> or C<false>.
+L<JSON::MaybeXS> does not allow unblessed references other than hash and array
+references or references to the integers C<0> and C<1>, and will throw an
+exception if attempting to encode one. L<Mojo::JSON> will treat scalar
+references as references to C<0> or C<1> and will encode them to C<true> or
+C<false>.
 
 =head2 Escapes
 
-L<Mojo::JSON> currently escapes the slash character C</> for security reasons, as well as the unicode characters C<u2028> and C<u2029>, while L<JSON::MaybeXS> does not. This does not affect decoding of the resulting JSON.
+L<Mojo::JSON> currently escapes the slash character C</> for security reasons,
+as well as the unicode characters C<u2028> and C<u2029>, while L<JSON::MaybeXS>
+does not. This does not affect decoding of the resulting JSON.
 
 =head2 inf and nan
 
-L<Mojo::JSON> encodes C<inf> and C<nan> to strings, whereas L<JSON::MaybeXS> will encode them as numbers (barewords) producing invalid JSON.
+L<Mojo::JSON> encodes C<inf> and C<nan> to strings, whereas L<JSON::MaybeXS>
+will encode them as numbers (barewords) producing invalid JSON.
 
 =head2 Upgraded Numbers
 
-L<JSON::MaybeXS> will attempt to guess if a value to be encoded is numeric or string based on its last usage. Therefore, using a variable containing C<13> in a string will cause it to be encoded as C<"13"> even if the variable itself was not changed. L<Mojo::JSON> will encode C<13> as C<13> regardless of whether it has been used as a string.
+L<JSON::MaybeXS> will attempt to guess if a value to be encoded is numeric or
+string based on its last usage. Therefore, using a variable containing C<13> in
+a string will cause it to be encoded as C<"13"> even if the variable itself was
+not changed. L<Mojo::JSON> will encode C<13> as C<13> regardless of whether it
+has been used as a string.
 
 =head1 BUGS
 
-This is a monkey-patch of one of a few possible modules into another, and they have incompatibilities, so there will probably be bugs. Report any issues on the public bugtracker.
+This is a monkey-patch of one of a few possible modules into another, and they
+have incompatibilities, so there will probably be bugs. Report any issues on
+the public bugtracker.
 
 =head1 AUTHOR
 
@@ -80,7 +104,8 @@ Sebastian Riedel, author of L<Mojolicious>, for basic implementation.
 
 Copyright 2014, Dan Book.
 
-This library is free software; you may redistribute it and/or modify it under the terms of the Artistic License version 2.0..
+This library is free software; you may redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
 
 =head1 SEE ALSO
 

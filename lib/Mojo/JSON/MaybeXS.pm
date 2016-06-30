e1f42d343820ddd75cb1f99a64489ce214e0e6fc
Dan Book <grinnz@grinnz.com>
2016-06-30 17:44:20 -0400

    Bump version

--- lib/Mojo/JSON/MaybeXS.pm
+++ lib/Mojo/JSON/MaybeXS.pm
@@ -6,7 +6,7 @@ use Mojo::Util 'monkey_patch';
 use JSON::MaybeXS 'JSON';
 use Mojo::JSON ();
 
-our $VERSION = '0.012';
+our $VERSION = '0.013';
 
 my $BINARY = JSON::MaybeXS->new(utf8 => 1, canonical => 1, allow_nonref => 1,
 	allow_unknown => 1, allow_blessed => 1, convert_blessed => 1);
